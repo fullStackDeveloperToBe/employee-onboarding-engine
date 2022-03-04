6927358d69511338964ca8ce3974e30d92e44cda
fullStackDeveloperToBe <gowdadushyanth27@gmail.com>
2022-03-04 09:06:52 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,13 @@
+class Array
+{
+	public static void main (String[] args)
+	{		
+	    int[] arr = new int[]{0,1,2,3,4,5,6,7,8,9,10 }; 
+	    
+		
+	// accessing the elements of the specified array
+	for (int val:arr[])
+		System.out.println("Element at index " + i +
+									" : "+ val[i]);		
+	}
+}
