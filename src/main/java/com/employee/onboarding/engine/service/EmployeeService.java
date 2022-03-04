f8efb9a4f439965065a73b0113fcc387826be5fe
fullStackDeveloperToBe <gowdadushyanth27@gmail.com>
2022-03-04 15:44:58 +0530

    added comment

--- src/main/java/com/employee/onboarding/engine/service/EmployeeService.java
+++ src/main/java/com/employee/onboarding/engine/service/EmployeeService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.employee.onboarding.engine.entity.Employee;
 import com.employee.onboarding.engine.repository.EmployeeRepository;
-
+//@Service file
 @Service
 public class EmployeeService {
 	@Autowired
