096eb4de6c720c8a770778c25c956cb8bfe64d47
fullStackDeveloperToBe <gowdadushyanth27@gmail.com>
2022-03-30 09:59:16 +0530

    command line runner

--- src/main/java/com/employee/onboarding/engine/EmployeeOnboardingEngineApplication.java
+++ src/main/java/com/employee/onboarding/engine/EmployeeOnboardingEngineApplication.java
@@ -1,14 +1,26 @@
 package com.employee.onboarding.engine;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.employee.onboarding.engine.utility.Logging;
+
 @SpringBootApplication
-public class EmployeeOnboardingEngineApplication {
+public class EmployeeOnboardingEngineApplication implements CommandLineRunner {
+	
+	@Autowired
+	Logging logUtil;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeOnboardingEngineApplication.class, args);
 	}
+	
+	@Override
+    public void run(String... args) {
+        logUtil.logInfo("EXECUTING : command line runner");
 
+}
 }
 
