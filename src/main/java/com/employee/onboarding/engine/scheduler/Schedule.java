c94e64d9b9cf70aa4e759d0f1aefa87cbccd7ddb
fullStackDeveloperToBe <99525378+fullStackDeveloperToBe@users.noreply.github.com>
2022-06-03 17:39:16 +0530

    scheduler update with cors

--- src/main/java/com/employee/onboarding/engine/scheduler/Schedule.java
+++ src/main/java/com/employee/onboarding/engine/scheduler/Schedule.java
@@ -1,26 +1,35 @@
 package com.employee.onboarding.engine.scheduler;
 
-import org.apache.logging.log4j.core.config.Scheduled;
+//import org.apache.logging.log4j.core.config.Scheduled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.employee.onboarding.engine.utility.Logging;
 
 @Configuration
 @EnableScheduling
-@Scheduled
 //(cron = "0 0 0 * * ?") every day at midnight
 public class Schedule {
 	
 	@Autowired
 	Logging logUtil;
 	
+	int i = 0;
+	
 	@Bean
 	public void getSchedule() {
 	logUtil.logInfo("This is Scheduling");
-	System.out.println("Fixed rate task - " + System.currentTimeMillis() / 1000);
+	System.out.println("CURRENT TIME Fixed rate task - " + System.currentTimeMillis() / 1000);
+	}
+	
+	
+	@Scheduled(cron = "* * 7 * * ?")
+	public void execute() 
+	{
+		logUtil.logInfo("This is Scheduling "+ i++);
 	}
 	
 }
